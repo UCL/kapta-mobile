e81f277bf93303870617618d316cbea5ba948807
acholyn <a.ho-lyn@ucl.ac.uk>
2024-10-31 15:42:12 +0000

    initial burger changes - toggling still not working

--- src/BurgerMenu.jsx
+++ src/BurgerMenu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import StatusBar from "./StatusBar.jsx";
 import "./styles/burger-menu.css";
@@ -10,6 +10,17 @@ export default function BurgerMenu({
 	setIsLoginVisible,
 	setIsWelcomeVisible,
 }) {
+	const [openSection, setOpenSection] = useState(null);
+
+	const toggleSection = (sectionId) => {
+		setOpenSection((prevOpenSection) => {
+			if (prevOpenSection === sectionId) {
+				return null; // Close the section if it's already open
+			} else {
+				return sectionId; // Open the new section, which automatically closes others
+			}
+		});
+	};
 	const { t } = useTranslation();
 
 	return (
@@ -25,35 +36,65 @@ export default function BurgerMenu({
 				setIsLoginVisible={setIsLoginVisible}
 				setIsWelcomeVisible={setIsWelcomeVisible}
 			/>
-			<details className="bm-item">
-				<summary>{t("about")}</summary>
-				<div
-					className="bm-item__content"
-					dangerouslySetInnerHTML={{ __html: t("aboutContent") }}
-				></div>
-			</details>
-			<details className="bm-item">
-				<summary>{t("people")}</summary>
-				<div
-					className="bm-item__content"
-					dangerouslySetInnerHTML={{ __html: t("peopleContent") }}
-				></div>
-			</details>
-			<div className="bm-item bm-links">
-				<a
-					href="https://github.com/UCL/kapta-mobile"
-					id="gh"
-					className="bm-item__content"
-				>
-					{GHIcon}
-				</a>
-				<a
-					href="https://github.com/UCL/kapta-mobile?tab=readme-ov-file#legal-disclaimer"
-					id="disclaimer"
-					className="bm-item__content"
-				>
-					{t("legalDisclaimer")}
-				</a>
+			<div className="bm__content">
+				<div>
+					<details className="bm__item" open={openSection === "about"}>
+						<summary onClick={() => toggleSection("about")}>
+							{t("about")}
+						</summary>
+						<div
+							className="bm__item__content"
+							dangerouslySetInnerHTML={{ __html: t("aboutContent") }}
+						></div>
+					</details>
+					<details className="bm__item" open={openSection === "why"}>
+						<summary onClick={() => toggleSection("why")}>Why Kapta</summary>
+						<div
+							className="bm__item__content"
+							dangerouslySetInnerHTML={{ __html: t("aboutContent") }}
+						></div>
+					</details>
+					<details className="bm__item" open={openSection === "what"}>
+						<summary onClick={() => toggleSection("what")}>
+							What's Next?
+						</summary>
+						<div
+							className="bm__item__content"
+							dangerouslySetInnerHTML={{ __html: t("aboutContent") }}
+						></div>
+					</details>
+					<details className="bm__item" open={openSection === "people"}>
+						<summary onClick={() => toggleSection("people")}>
+							{t("people")}
+						</summary>
+						<div
+							className="bm__item__content"
+							dangerouslySetInnerHTML={{ __html: t("peopleContent") }}
+						></div>
+					</details>
+				</div>
+				<div className="links-disclaimer__wrapper">
+					<div className="bm__item">
+						<a
+							href="https://github.com/UCL/kapta-mobile"
+							id="gh"
+							className="bm__item__content"
+						>
+							{GHIcon}
+						</a>
+					</div>
+					<p className="bm__item" id="disclaimer">
+						Disclaimer: The Kapta team has made every effort to develop an app
+						that enables users to create WhatsApp Maps with the highest possible
+						accuracy. However, we cannot accept responsibility for any errors,
+						omissions, or inconsistencies that may occur. If you encounter any
+						issues or have feedback, please reach out to us at{" "}
+						<a href="mailto:geog.excites@ucl.ac.uk?subject=Kapta Mobile Feedback">
+							geog.excites@ucl.ac.uk
+						</a>{" "}
+						or via <a href="ASK_URL">WhatsApp</a> at +34 678380944.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
